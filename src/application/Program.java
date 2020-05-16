fc158e700fb30bd231d36907c509e7a74996741f
Luiz <lhserafim@gmail.com>
2020-05-16 09:27:55 -0300

    Projeto atualizado. Em construção. Solução sem interfaces

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,47 @@
 package application;
 
+import model.entities.CarRental;
+import model.entities.Vehicle;
+import model.services.BrazilTaxService;
+import model.services.RentalServices;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Locale;
 import java.util.Scanner;
 
 public class Program {
-    public static void main(String args[]) {
+    public static void main(String args[]) throws ParseException {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:ss");
+        System.out.println("Enter Rental Data:");
+        System.out.print("Car model: ");
+        String carModel = sc.nextLine();
+        System.out.print("Pickup: ");
+        Date start = sdf.parse(sc.nextLine());
+        System.out.print("Return: ");
+        Date finish = sdf.parse(sc.nextLine());
+
+        // Instanciar a classe de aluguel de carro
+        CarRental cr = new CarRental(start, finish, new Vehicle(carModel));
+
+        System.out.print("Enter price per hour: ");
+        double pricePerHour = sc.nextDouble();
+        System.out.print("Enter price per day: ");
+        double pricePerDay = sc.nextDouble();
+
+        // Instanciar o serviço de aluguel de carro
+        RentalServices rentalService = new RentalServices(pricePerHour, pricePerDay, new BrazilTaxService());
+
+        // vou acessar o meu serviço
+        rentalService.processInvoice(cr);
+
+        System.out.println("INVOICE: ");
+        System.out.println("Basic payment: " + String.format("%.2f", cr.getInvoice().getBasicPayment()));
+        System.out.println("Tax: " + String.format("%.2f", cr.getInvoice().getTax()));
+        System.out.println("Total payment: " + String.format("%.2f", cr.getInvoice().getTotalPayment()));
 
         sc.close();
     }
